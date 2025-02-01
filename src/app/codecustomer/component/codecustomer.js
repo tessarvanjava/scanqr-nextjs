1e36ce17b0da8ef10f0dc44c4ead45c2547dc460
tessar-MSI <tessar2008@gmail.com>
2025-02-01 09:28:28 +0700

    add currency on codecustomer

--- src/app/codecustomer/component/codecustomer.js
+++ src/app/codecustomer/component/codecustomer.js
@@ -2,12 +2,13 @@
 
 import { Button, Container, Table, Form, Alert } from 'react-bootstrap'
 import useSWR, { mutate, useSWRConfig } from 'swr'
-import moment from 'moment'
+import moment, { locale } from 'moment'
 import 'moment/locale/id'
 import Loading from '../../scan/module/loading.js'
 import token from '../../scan/module/token.js'
 import axios from 'axios'
 import { useSearchParams } from 'next/navigation.js'
+import defaultCurrency from 'currency-formatter'
 
 const fetcher = (url) => axios.get(url, { headers: { sign: token } }).then((res) => res.data.data)
 
@@ -24,7 +25,13 @@ const returnUnit = (a) => {
   }
 }
 
+function currency(value) {
+  return defaultCurrency.format(value, { locale: 'id-ID'})
+}
+
 function CodeCustomer() {
+
+
   const searchParams = useSearchParams()
   const id = searchParams.get('id')
   const url = `${process.env.api}/api/order/${id}`
@@ -59,7 +66,7 @@ function CodeCustomer() {
               <td>{item.id_pemesanan}</td>
               <td>{item.nama}</td>
               <td>{item.unit}</td>
-              <td>{item.total_bayar}</td>
+              <td>{currency(item.total_bayar)}</td>
               <td>{returnUnit(item.ambil_tgl)}</td>
               <td>{item.ambil_jam}</td>
               <td>{returnUnit(item.kembali_tgl)}</td>
